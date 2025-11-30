e4281d3ed97b94ccf67c9d59db8f9ce233dd8109
Michał Sadkowski <msadkowski000@gmail.com>
2025-11-30 14:22:46 +0100

    created base for angular app

--- Web services architecture/brand-service/src/main/java/lab/aui/brand_service/BrandServiceApplication.java
+++ Web services architecture/brand-service/src/main/java/lab/aui/brand_service/BrandServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class BrandServiceApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(BrandServiceApplication.class, args);
 	}
 
